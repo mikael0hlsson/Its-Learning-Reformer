6a794bda49ca5628095ae5d9123ba060ab6aa9ca
unknown <tsmioh@WCCNU1521X71.ad.mah.se>
2013-09-16 15:33:13 +0200

    Restructured assignmentReport.js for easyer development. Changed labes for resubmit and needs grading. Check duplicate names added to the injection (now there is a warning).

--- src/its.assignmentReport.js
+++ src/its.assignmentReport.js
@@ -11,33 +11,88 @@ if(chrome.extension)
 
 $(function () {
     var isColumnAdded=false;
+    var num = $('.pagesize').html().match(/\d+/g)[2]
+    var location = document.location.href;    
+    var mainTable = $('table tbody');
+    var showButton=true;
+    var From = 0;
+    var passingGrades=[
+        "A", "B", "C", "D", "E", //International
+        "G", "VG" , "MVG", //Swedish
+        "Klar"  
+    ];
+
+    var nonPassingGrades=[
+        "F", "U"
+    ];
+
+    var hasNotDoneAnythingAverageGradeIdentifyers=[
+        "(U)", "(F)", "F/(F)", "U/(U)"
+    ];
+    var mixedGradesIdentifyers=[
+        "** "
+    ];
+
+    var resubmitIdentifyers=[
+        "Needs", "rättad"
+    ];
+    var resubmitText="RE";
+    var resubmitTitle="They need to resubmit";
+    
+    var needsGradingIdentifyers=[
+        "Resub", "skicka"
+    ];
+    var needsGradingText="NG";
+    var needsGradingTitle="They have resubmitted. Needs grading.";
+
+    var textsToReplaceWithG=[
+        "Godkänd"
+    ];
+
+    var cellsToClear=[
+        "Inte", "sub", "No assessment"
+    ];
+    var peopleList=[];
 
     function addPersonalNumberColumn(){
         if(isColumnAdded)
             return;
         $(".tablelisting table tr:lt(1)").prepend("<th>Personnummer</th>");
         $(".tablelisting table tr:gt(0)").prepend("<td>#</td>");
+        addPeopleToList(); //needs to be done once.. so here is a good place... (since the column needs to be added)
         isColumnAdded=true;
     }
 
-
-    var num = $('.pagesize').html().match(/\d+/g)[2]
-
-    var location = document.location.href;
-    if (location.indexOf('&') != -1)
-        location = location.substr(0, location.indexOf('&')); //remove extra parameters
-
-    var mainTable = $('table tbody');
+    function addPeopleToList(){
+         mainTable.find('tr').find('td:nth-child(2)').each(function (i, td) {
+            var name = $(this).text().replace(',', '');
+            peopleList.push(name);
+        });
+    }
 
     function hideAverageGradeColumn(){
         mainTable.find("tr:lt(1) th:last-child").hide();
         mainTable.find("tr:gt(0) td:last-child").hide();
     }
 
+   
+    if (location.indexOf('&') != -1)
+        location = location.substr(0, location.indexOf('&')); //remove extra parameters
 
     $('tr:not(.header)', mainTable).remove(); //clear the table    
-    var showButton=true;
-    var From = 0;
+    
+    function findCellsThatContains(rows, identifyers){
+        var question="";
+        for(var i=0;i<identifyers.length;i++)
+        {
+            question+='td:contains("'+identifyers[i]+'")';
+            if(i<identifyers.length-1) question+=", ";
+        }
+       
+        return rows.find(question);
+    }
+
+
     while (num > 0) {
         $.ajax({
             url: location,
@@ -46,32 +101,41 @@ $(function () {
                 var rows = $('table tr:not(.header)', d);
 
                 rows.find('td:first').attr('nowrap', '');
-                rows.find('td:contains("Needs"), td:contains("rättad")').text('RE').attr('title','They have resubmitted');
-                rows.find('td:contains("skicka"), td:contains("Resub")').text('WTG').attr('title','They need to resubmit');
                 
-               
+                findCellsThatContains(rows, needsGradingIdentifyers).text(needsGradingText).attr('title',needsGradingTitle);
+                findCellsThatContains(rows, resubmitIdentifyers).text(resubmitText).attr('title', resubmitTitle);
+                
                 rows.find('td span').remove(); //remove the keywords (Klar, ...)
-                rows.find('a').replaceWith(function (a) { return $(this).text(); }); //remove links
+                //rows.find('a').replaceWith(function (a) { return $(this).text(); }); //remove links ... why? link is destroyed anyway...
                 
 
-                rows.find('td:last:contains("(F)"), td:last:contains("(U)")').each(function (i, td) {
+                //Decorate all rows that have not an avarage of pass with incomplete
+                //Might be usefull.. not used(???)
+                findCellsThatContains(rows, nonPassingGrades).each(function (i, td) {
                     $(td).closest('tr').addClass('incomplete');
                 });
+
+                //Decorate all rows with people who has not submited anything
                 rows.find('td:last').each(function (i, td) {
                     var el = $(td);
-                    if (el.text() == "(U)" || el.text() == "(F)" || el.text() == "F/(F)" || el.text() == "U/(U)"|| el.text().indexOf('0 Godkänd, 0') >= 0) {
+                    if($.inArray(el.text(), hasNotDoneAnythingAverageGradeIdentifyers)!=-1 || el.text().indexOf('0 Godkänd, 0') >= 0)
                         $(td).closest('tr').addClass('idiot');
-                    }
-                    if(el.text() == "**")
-                    {
+                    //Check if the grades are mixed.. and if.. diable the button
+                    if($.inArray(el.text(), mixedGradesIdentifyers))
                         showButton=false;
-                    }
+                    
                 });
-                rows.find('td:contains("Inte"), td:contains("sub"), td:contains("No assessment")').text(''); //Needs to be done here else courses with grades like "0 Godkänd, 0/(5) Inte godkänd" wont be marked
+                findCellsThatContains(rows,cellsToClear).text(''); //Needs to be done here else courses with grades like "0 Godkänd, 0/(5) Inte godkänd" wont be marked
+                //remove * from everywhere
                 rows.find('td:contains("*")').text(function (a) { return $(this).text().replace("*", ""); });// same as above
+                
+                //Should pehaps be made more general???
                 rows.find('td').each(function() {  //Courses with only Godkänt... replace with G
-                    var text = $(this).text().replace('Godkänd', 'G');
-                    $(this).text(text);
+                    for(var index=0; index<textsToReplaceWithG.length;index++)
+                    {
+                        var text = $(this).text().replace(textsToReplaceWithG[index], 'G');
+                        $(this).text(text);
+                    }
                 });
                 mainTable.append(rows);
 
@@ -91,7 +155,12 @@ $(function () {
     }else{
         $('<label style="float:right">Mixed grades. Cant toggle people.</label>').prependTo($('body'));
     }
+
+
+    var personWithSameNameWarning="## Duplicate name ##";
+    //Add button for injecting personalnumbers
     $('<button class="login-button">Inject Personal Numbers</button>').click(function () {
+        
         addPersonalNumberColumn();
         var str = $('textarea').val();
 
@@ -103,14 +172,21 @@ $(function () {
 
         mainTable.find('tr').find('td:nth-child(2)').each(function (i, td) {
             var name = $(this).text().replace(',', '');
-            for (var i = 0; i < people.length; i++) {
-                if (name.toLowerCase() == people[i][1].toLowerCase()) {
-                    if($(this).prev().text()=='#' || $(this).prev().text()==people[i][0])
-                        $(this).prev().text(people[i][0]);
-                    else
-                        alert('Warning! \nThe personal number was different that the one inserted\n for the user: '+$(this).text());
-                    return;
+            var numberOfpeoplewithThatName=0;
+            $.each(peopleList, function(i, item) {
+                if (item == name)
+                    numberOfpeoplewithThatName++;
+            });
+
+            if(numberOfpeoplewithThatName==1){
+                for (var i = 0; i < people.length; i++) {
+                    if (name.toLowerCase() == people[i][1].toLowerCase()) {
+                        if($(this).prev().text()=='#' || $(this).prev().text()==people[i][0])
+                            $(this).prev().text(people[i][0]);
+                    }
                 }
+            }else if(numberOfpeoplewithThatName>1){
+                $(this).prev().text(personWithSameNameWarning).css('background-color', 'red');
             }
         });
 
@@ -121,16 +197,16 @@ $(function () {
 
     mainTable.find('td').each(function (i, td) {
         var text = $(td).text().trim();
-        if (text == "VG" || text == "A" || text == "B" || text == "G" || text == "C" || text == "Klar") {
+        if ($.inArray(text, passingGrades)!=-1) {
             $(td).addClass('colorbox_green');
         }
         //if (text == "G" || text == "C") {
         //    $(td).html($('<span/>', { text: text, class: 'colorbox_blue' }));
         //}
-        if (text == "RE") {
+        if (text == needsGradingText) {
             $(td).css('background-color', 'red'); //Brighter warning
         }
-        if (text == "WTG") {
+        if (text == resubmitText) {
             $(td).addClass('colorbox_yellow');
         }
     });
